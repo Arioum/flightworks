19d672257ff48d11b25c5a6179a67777f014275f
arioum <amoghrao121@gmail.com>
2024-01-15 01:13:56 +0530

    added notfound page

---
+++ client/app/routes/$notFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <main>
+      <section className='content-wrapper h-[100%]'>
+        <h1 className='flex flex-col justify-center items-center mt-[4rem] text-[1.6rem]'>
+          <span className="text-[2rem] font-[700]">404 Not Found</span>
+          This page doesnt seem to exist.
+        </h1>
+      </section>
+    </main>
+  );
+};
+
+export default NotFound;
--- client/app/routes/usage.jsx
+++ client/app/routes/usage.jsx
@@ -4,18 +4,18 @@ const usage = () => {
       <h1 className='text-[1.8rem] mt-[2em] font-[500]'>Usage Limitatons</h1>
       <hr className='py-[1em]' />
       <ul className='flex flex-col gap-[1em]'>
-        <li>
-          The platform allows for up to 5 users to create an account. This limit
-          is set because the free API enforces a minimum of 50 requests/month
-          limit.
-        </li>
+        <li>The platform allows for up to 5 users to create an account.</li>
         <li>
           The user will only need to create an account if they need to view the
           plane schedules.
         </li>
         <li>
+          This limit is set because the free API enforces a minimum of 50
+          requests/month limit.
+        </li>
+        <li>
           This limit is set by the project maintainer, if you wish to try the
-          app, please reach out to them.
+          feature, please contact them.
         </li>
       </ul>
     </main>
