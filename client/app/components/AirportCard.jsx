9b335984976c8d081d6ec52d47c507e9df429546
arioum <amoghrao121@gmail.com>
2024-01-15 01:14:10 +0530

    added airport card

--- client/app/components/AirportCard.jsx
+++ client/app/components/AirportCard.jsx
@@ -1,22 +1,59 @@
-const AirportCard = ({ airport }) => {
-  console.log(airport);
+import {
+  FaPhone,
+  FaLocationDot,
+  FaLocationArrow,
+  FaList,
+} from 'react-icons/fa6';
+import Button from './Button';
 
+const AirportCard = ({ airport }) => {
   const { latitude, longitude } = airport;
 
-
   return (
-    <section>
-      <h1>{airport.name}</h1>
-      <p>{airport.iata}</p>
-      <p>{airport.icao}</p>
-      <p>{airport.location}</p>
-      <p>{airport.country}</p>
-      <p>{airport.country_iso}</p>
-      <p>{airport.county}</p>
-      <p>{airport.state}</p>
-      <p>{airport.phone}</p>
-      <p>{airport.postal_code}</p>
-      <p>{airport.uct}</p>
+    <section className='border-[2px] rounded-[1em] p-[1rem]'>
+      <h1 className='text-[2rem] font-[700] md:text-[1.4rem]'>
+        {airport.name}({airport.iata}/{airport.icao})
+      </h1>
+      <p className='text-[1.2rem] font-[600] flex items-center gap-[.4em] text-[#505050] md:text-[1rem]'>
+        <FaLocationDot />
+        {airport.location} ({airport.country_iso})
+      </p>
+      <div className='flex flex-col py-[1em]'>
+        {airport.county && <p>District/County: {airport.county}</p>}
+        <p>Country: {airport.country}</p>
+        <p>State: {airport.state}</p>
+        {airport.postal_code && <p>Postal Code: {airport.postal_code}</p>}
+      </div>
+      <p className='font-[600] flex items-center gap-[.4em] bg-[#ececec] max-w-fit px-[.8em] py-[.4em] rounded-[50em]'>
+        <FaPhone />
+        {airport.phone}
+      </p>
+      <div className='flex gap-[.6em] mt-[1em] md:flex-col md:gap-0'>
+        <Button
+          type={'link'}
+          dest={`https://www.google.com/maps/@${latitude},${longitude},15z`}
+          target={'blank'}
+          variant={'secondary'}
+          styles={
+            'flex items-center justify-center gap-[.5em] max-w-fit px-0 mt-[10px] md:max-w-[100%]'
+          }
+        >
+          <FaLocationArrow />
+          View in Maps
+        </Button>
+        <Button
+          type={'link'}
+          dest={``}
+          target={'blank'}
+          variant={'primary'}
+          styles={
+            'flex items-center justify-center gap-[.5em] max-w-fit px-0 mt-[10px] md:max-w-[100%]'
+          }
+        >
+          <FaList />
+          View Schedule
+        </Button>
+      </div>
     </section>
   );
 };
