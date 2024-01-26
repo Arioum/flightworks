7e7c19d01a42f948ee0aab44e75a5d27dd172442
arioum <amoghrao121@gmail.com>
2024-01-26 16:33:42 +0530

    moved airport header section to its layout comp

---
+++ client/app/layouts/AirportHeader.jsx
@@ -0,0 +1,33 @@
+import { FaLocationDot } from 'react-icons/fa6';
+
+const AirportHeader = ({ airport, img }) => {
+  return (
+    <section
+      className={`max-w-[100%] rounded-[10px] p-[1em] mt-[1em] min-h-[200px] max-h-[330px] flex gap-[1em] justify-between items-center bg-[#202020] overflow-hidden ${
+        img ? 'md:flex-col' : 'md:flex-row md:justify-center'
+      }`}
+    >
+      <div className='md:order-2'>
+        <h1
+          className='text-[2rem] text-[#fff] font-[700] md:text-[1.4rem]'
+          suppressHydrationWarning={true}
+        >
+          {airport.name} ({airport.iata}/{airport.icao})
+        </h1>
+        <p className='text-[1.2rem] font-[600] flex items-center md:justify-center gap-[.4em] text-[#e2e2e2] md:text-[1rem]'>
+          <FaLocationDot />
+          {airport.location} ({airport.country_iso})
+        </p>
+      </div>
+      {img && (
+        <img
+          src={img}
+          alt='airport-image'
+          className='relative right-0 max-h-[220px] overflow-hidden md:order-1 max-w-[100%] rounded-[6px]'
+        />
+      )}
+    </section>
+  );
+};
+
+export default AirportHeader;
--- client/app/routes/schedule.$airport.jsx
+++ client/app/routes/schedule.$airport.jsx
@@ -1,10 +1,10 @@
 import { useRecoilValue } from 'recoil';
 import { airportAtom } from '../store/atom/airport';
-import { FaLocationDot } from 'react-icons/fa6';
 import { Outlet, useLocation } from '@remix-run/react';
 import GeneralTab from '../layouts/GeneralTab';
 import airportData from '../airportDetails.json';
 import AirportTabs from '../layouts/AirportTabs';
+import AirportHeader from '../layouts/AirportHeader';
 
 const SchedulePage = () => {
   const airport = useRecoilValue(airportAtom);
@@ -19,25 +19,7 @@ const SchedulePage = () => {
 
   return (
     <main className='content-wrapper md:px-[1em]'>
-      <section className='max-w-[100%] rounded-[10px] p-[1em] mt-[1em] max-h-[330px] flex gap-[1em] justify-between items-center bg-[#202020] overflow-hidden md:flex-col'>
-        <div className='md:order-2'>
-          <h1
-            className='text-[2rem] text-[#fff] font-[700] md:text-[1.4rem]'
-            suppressHydrationWarning={true}
-          >
-            {airport.name} ({airport.iata}/{airport.icao})
-          </h1>
-          <p className='text-[1.2rem] font-[600] flex items-center md:justify-center gap-[.4em] text-[#e2e2e2] md:text-[1rem]'>
-            <FaLocationDot />
-            {airport.location} ({airport.country_iso})
-          </p>
-        </div>
-        <img
-          src={img}
-          alt=''
-          className='relative right-0 max-h-[220px] overflow-hidden md:order-1 max-w-[100%] rounded-[6px]'
-        />
-      </section>
+      <AirportHeader airport={airport} img={img} />
       <section className='mb-[2em]'>
         <AirportTabs
           activeIata={activeIata}
