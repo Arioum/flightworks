031f785d6511c74ae32add4348d46185d356df49
arioum <amoghrao121@gmail.com>
2024-01-30 20:24:40 +0530

    added process env

--- client/app/layouts/SearchAirport.jsx
+++ client/app/layouts/SearchAirport.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Outlet } from '@remix-run/react';
+import { useSetRecoilState } from 'recoil';
+import { airportAtom } from '../store/atom/airport';
 import { useNavigate } from '@remix-run/react';
 import axios from 'axios';
 
-const SearchAirport = () => {
+const SearchAirport = ({ apiKey }) => {
   const [airportInput, setAiportInput] = useState('');
-  const [matchedAirport, setMatchedAirport] = useState(null);
+  const setAirportData = useSetRecoilState(airportAtom);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,16 +17,15 @@ const SearchAirport = () => {
         url: 'https://airport-info.p.rapidapi.com/airport',
         params: { iata: airportInput },
         headers: {
-          'X-RapidAPI-Key': '',
+          'X-RapidAPI-Key': apiKey,
           'X-RapidAPI-Host': 'airport-info.p.rapidapi.com',
         },
       };
 
       try {
         const response = await axios.request(options);
-        console.log(response.data);
         if (!response.data.error) {
-          setMatchedAirport(response.data);
+          setAirportData(response.data);
           navigate(`./${airportInput}`);
         }
       } catch (error) {
@@ -35,15 +36,15 @@ const SearchAirport = () => {
   }, [airportInput]);
 
   return (
-    <section className='content-wrapper py-[2em]'>
-      <h1 className='text-2xl font-[600]'>SearchAirport</h1>
+    <section>
+      <h1 className='text-2xl font-[600] mb-[.5em]'>SearchAirport</h1>
       <input
         type='text'
         onChange={(e) => setAiportInput(e.target.value)}
         placeholder='Enter IATA code'
         className='px-4 py-2 border-2 rounded-[5px] focus:outline-none'
       />
-      <Outlet context={matchedAirport} />
+      <Outlet />
     </section>
   );
 };
--- client/app/routes/search.jsx
+++ client/app/routes/search.jsx
@@ -1,9 +1,17 @@
+import { useLoaderData } from '@remix-run/react';
 import SearchAirport from '../layouts/SearchAirport';
 
+export const loader = async ({ req, params }) => {
+  const apiKey = process.env.RAPID_API_KEY_AIRPORT;
+  return apiKey;
+};
+
 const Search = () => {
+  const apiKey = useLoaderData();
+
   return (
     <main className='content-wrapper py-[2em] md:px-[1em]'>
-      <SearchAirport />
+      <SearchAirport apiKey={apiKey} />
     </main>
   );
 };
