5a8c2ca59f3111974dc2cb5966bc41c7f5b36800
arioum <amoghrao121@gmail.com>
2024-03-26 13:18:15 +0530

    added loading to avoid hmr errors

--- client/app/routes/search.$airport.jsx
+++ client/app/routes/search.$airport.jsx
@@ -1,15 +1,22 @@
 import { memo } from 'react';
 import { useRecoilValue } from 'recoil';
-import AirportCard from '../components/AirportCard';
 import { airportAtom } from '../store/atom/airport';
+import AirportCard from '../components/AirportCard';
+import Loader from '../components/Loader';
 
 const Airport = memo(() => {
   const airportDetails = useRecoilValue(airportAtom);
 
   return (
     <>
-      <h6 className='my-[1em] font-[600] text-[1.6rem]'>Airport Found</h6>
-      <AirportCard airport={airportDetails} />
+      {airportDetails ? (
+        <div>
+          <h6 className='my-[1em] font-[600] text-[1.6rem]'>Airport Found</h6>
+          <AirportCard airport={airportDetails} />
+        </div>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 });
